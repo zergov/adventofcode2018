d902875b287901952caf11101d26cd74c8069f7d
Jonathan Lalande <jonathan.lalande@shopify.com>
2018-12-13 18:18:15 -0500

    day 8

--- day8/solution.rb
+++ day8/solution.rb
@@ -1,12 +1,37 @@
 #!/usr/bin/env ruby
 
-def part1
-  input = File.read('input.txt')
+class Node
+  attr_accessor :metadata, :children
+  def initialize
+    @metadata = []
+    @children = []
+  end
+
+  def value
+    if @children.empty?
+      @metadata.reduce(:+)
+    else
+      @metadata
+        .map { |index| @children[index - 1] }
+        .compact
+        .reduce(0) { |sum, node| sum + node.value }
+    end
+  end
 end
 
-def part2
-  input = File.read('input.txt')
+
+def extract_metadata(stream)
+  child_count, metadata_count = stream.shift(2)
+
+  node = Node.new
+  child_count.times do |n|
+    child_node = extract_metadata(stream)
+    node.children << child_node
+  end
+
+  node.metadata = stream.shift(metadata_count)
+  return node
 end
 
-puts "Solution part1: #{part1}"
-puts "Solution part2: #{part2}"
+stream = File.read('input.txt').split(' ').map(&:to_i)
+p extract_metadata(stream).value
