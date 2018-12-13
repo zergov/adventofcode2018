b4019406ecd592d3f0322a16243f416a29012e87
Jonathan Lalande <jonathan.lalande@shopify.com>
2018-12-13 00:35:37 -0500

    day 8

---
+++ day8/solution.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def part1
+  input = File.read('input.txt')
+end
+
+def part2
+  input = File.read('input.txt')
+end
+
+puts "Solution part1: #{part1}"
+puts "Solution part2: #{part2}"
