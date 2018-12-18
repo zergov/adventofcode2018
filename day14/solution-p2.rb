97352ea9ef25edc20ba9f1eb2c8acf6d81aac42b
Jonathan Lalande <jonathan.lalande@shopify.com>
2018-12-18 10:46:05 -0500

    day14 p2

--- day14/solution-p2.rb
+++ day14/solution-p2.rb
@@ -1,15 +1,13 @@
-pattern = "074501"
 scoreboard = [3, 7]
 elfs = [0, 1]
 
-until scoreboard.last(pattern.size).join == pattern do
+while scoreboard.length <= 25_000_000
   # create the new receipe
   created = elfs.map {|index| scoreboard[index]}.reduce(:+)
   # add receipe to scoreboard
-  scoreboard = scoreboard.concat(created.to_s.split('').map(&:to_i))
+  scoreboard.push(*(created.to_s.split('').map(&:to_i)))
   # update elfs index
-  elfs = elfs.map {|index| (index + scoreboard[index] + 1) % scoreboard.size }
+  elfs.map! {|index| (index + scoreboard[index] + 1) % scoreboard.size }
 end
 
-scoreboard.pop(pattern.size)
-p scoreboard.size
+p scoreboard.map(&:to_s).join =~ /074501/
